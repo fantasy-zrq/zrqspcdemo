7c157959b6c6d2a497b7db62dac1992ccad2c4b0
fantasy-zrq <384130598@qq.com>
2025-08-07 16:31:13 +0800

    refactor:改善gateway网关逻辑

--- gateway/src/main/java/com/example/gateway/filter/TokenValidateGatewayFilterFactory.java
+++ gateway/src/main/java/com/example/gateway/filter/TokenValidateGatewayFilterFactory.java
@@ -1,19 +1,21 @@
 package com.example.gateway.filter;
 
 import com.alibaba.fastjson2.JSONObject;
-import com.auth0.jwt.interfaces.DecodedJWT;
 import com.example.gateway.config.WhitePathConfig;
 import com.example.gateway.result.Result;
 import com.example.gateway.utils.JwtUtil;
 import org.springframework.cloud.gateway.filter.GatewayFilter;
 import org.springframework.cloud.gateway.filter.factory.AbstractGatewayFilterFactory;
 import org.springframework.core.io.buffer.DataBuffer;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.server.RequestPath;
 import org.springframework.http.server.reactive.ServerHttpRequest;
 import org.springframework.stereotype.Component;
 import reactor.core.publisher.Mono;
+
+import java.util.Map;
 
 /**
  * @author zrq
@@ -23,9 +25,12 @@ import reactor.core.publisher.Mono;
 @Component
 public class TokenValidateGatewayFilterFactory extends AbstractGatewayFilterFactory<WhitePathConfig> {
 
+    private final StringRedisTemplate stringRedisTemplate;
+
     //必须手动将Config文件传给父类
-    public TokenValidateGatewayFilterFactory() {
+    public TokenValidateGatewayFilterFactory(StringRedisTemplate stringRedisTemplate) {
         super(WhitePathConfig.class);
+        this.stringRedisTemplate = stringRedisTemplate;
     }
 
     /**
@@ -37,16 +42,15 @@ public class TokenValidateGatewayFilterFactory extends AbstractGatewayFilterFact
             ServerHttpRequest request = exchange.getRequest();
             RequestPath requestPath = request.getPath();
             if (!isWhitePath(config, requestPath.toString())) {
-                String token = request.getHeaders().getFirst("token");
-                DecodedJWT decodedJWT = JwtUtil.getTokenInfo(token);
-                String username = decodedJWT.getClaim("username").toString();
-                String userId = decodedJWT.getClaim("userId").toString();
-                if (!("zrq".equals(username) && "firstOne".equals(userId))) {
+                String token = request.getHeaders().getFirst("Authorization");
+                Map<String, String> tokenPayload = JwtUtil.verifyToken(token);
+                String username = tokenPayload.get("username");
+                if (stringRedisTemplate.opsForValue().get("zrq:spc:model:user:user-login:token:" + username) == null) {
                     exchange.getResponse().setStatusCode(HttpStatus.UNAUTHORIZED);
                     exchange.getResponse().getHeaders().setContentType(MediaType.APPLICATION_JSON);
                     Result result = Result.builder()
                             .code(HttpStatus.UNAUTHORIZED.value())
-                            .message("fail")
+                            .message("token校验失败")
                             .success(Boolean.FALSE)
                             .build();
                     DataBuffer buffer = exchange.getResponse().bufferFactory().wrap(JSONObject.toJSONString(result).getBytes());
--- gateway/src/main/java/com/example/gateway/utils/JwtUtil.java
+++ gateway/src/main/java/com/example/gateway/utils/JwtUtil.java
@@ -1,11 +1,10 @@
 package com.example.gateway.utils;
 
-import com.auth0.jwt.JWT;
-import com.auth0.jwt.JWTCreator;
-import com.auth0.jwt.algorithms.Algorithm;
-import com.auth0.jwt.interfaces.DecodedJWT;
+import cn.hutool.json.JSONObject;
+import cn.hutool.json.JSONUtil;
+import cn.hutool.jwt.JWT;
 
-import java.util.Calendar;
+import java.util.Date;
 import java.util.Map;
 
 /**
@@ -15,26 +14,36 @@ import java.util.Map;
  */
 public final class JwtUtil {
 
-    private static final String SIGNATURE = "x2n3y9un9c2n37984385782!*^@&#(*";
+    // 密钥（实际项目中建议从配置文件读取，且定期更换）
+    private static final String SECRET_KEY = "9f1099fa-c9eb-44b8-8d71-5ddb016873a7";
 
-    public static String getToken(Map<String, String> map) {
-        Calendar instance = Calendar.getInstance();
-        instance.add(Calendar.DATE, 7);
-        JWTCreator.Builder builder = JWT.create();
-        map.forEach(builder::withClaim);
-        builder.withExpiresAt(instance.getTime());
-        return builder.sign(Algorithm.HMAC256(SIGNATURE));
+    // 令牌过期时间（单位：毫秒），这里设置为一周小时
+    private static final long EXPIRE_TIME = 7 * 24 * 60 * 60 * 1000;
+
+    /**
+     * 产生token默认一周时间
+     *
+     * @param payloadMap jwt需要添加的负载
+     * @return 产生的token
+     */
+    public static String generateToken(Map<String, String> payloadMap) {
+        return JWT.create()
+                .addPayloads(payloadMap)
+                .setKey(SECRET_KEY.getBytes())
+                .setExpiresAt(new Date(System.currentTimeMillis() + EXPIRE_TIME))
+                .sign();
     }
 
-//    /**
-//     * token异常直接报错，不需要自己定义报错逻辑
-//     */
-//    public static void verifyToken(String token) {
-//        JWT.require(Algorithm.HMAC256(SIGNATURE)).build().verify(token);
-//    }
-
-
-    public static DecodedJWT getTokenInfo(String token) {
-        return JWT.require(Algorithm.HMAC256(SIGNATURE)).build().verify(token);
+    /**
+     * 解析jwt
+     *
+     * @param token 需要解析的令牌
+     * @return 返回令牌的负载部分
+     */
+    public static Map<String, String> verifyToken(String token) {
+        JSONObject payloads = JWT.of(token)
+                .getPayloads();
+        String jsonStr = JSONUtil.toJsonStr(payloads);
+        return JSONUtil.toBean(jsonStr, Map.class);
     }
 }
