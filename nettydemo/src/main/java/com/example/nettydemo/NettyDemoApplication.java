08d6a014278982175b462ae104750ff8c82d1ffd
fantasy-zrq <384130598@qq.com>
2025-08-07 19:08:31 +0800

    init：netty模块自学习

---
+++ nettydemo/src/main/java/com/example/nettydemo/NettyDemoApplication.java
@@ -0,0 +1,14 @@
+package com.example.nettydemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author zrq
+ */
+@SpringBootApplication
+public class NettyDemoApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(NettyDemoApplication.class, args);
+    }
+}
