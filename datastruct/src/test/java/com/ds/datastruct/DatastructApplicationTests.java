950a2492c4aa475f553750c10827e1500f9d7c66
fantasy-zrq <384130598@qq.com>
2025-09-27 17:04:51 +0800

    ds:初始化

---
+++ datastruct/src/test/java/com/ds/datastruct/DatastructApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ds.datastruct;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DatastructApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
