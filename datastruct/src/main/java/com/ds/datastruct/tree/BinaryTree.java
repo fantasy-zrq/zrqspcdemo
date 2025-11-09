e5bfee590b77a61464bc3aaaab9b18cafab3d13b
fantasy-zrq <384130598@qq.com>
2025-11-09 14:58:02 +0800

    ds:代码随想录-二叉树6、7、8

--- datastruct/src/main/java/com/ds/datastruct/tree/BinaryTree.java
+++ datastruct/src/main/java/com/ds/datastruct/tree/BinaryTree.java
@@ -12,14 +12,104 @@ import java.util.*;
 public class BinaryTree {
     public static void main(String[] args) {
         TreeNode head = new TreeNode(1);
-        TreeNode node1 = new TreeNode(2);
-        TreeNode node2 = new TreeNode(3);
-        head.right = node1;
-        node1.left = node2;
+        TreeNode node2 = new TreeNode(2);
+        TreeNode node3 = new TreeNode(3);
+        TreeNode node4 = new TreeNode(4);
+        TreeNode node5 = new TreeNode(5);
+        TreeNode node6 = new TreeNode(6);
+        TreeNode node7 = new TreeNode(7);
+        head.left = node2;
+        head.right = node3;
+        node2.left = node4;
+        node2.right = node5;
+        node3.left = node6;
+        node3.right = node7;
 //        List<Integer> res = rightSideView(head);
 //        res.forEach(System.out::println);
-        int res = minDepth(head);
+//        int res = minDepth(head);
+//        System.out.println("res = " + res);
+//        TreeNode res = invertTree(head);
+//        System.out.println("res = " + res);
+        boolean res = isSymmetric(head);
         System.out.println("res = " + res);
+    }
+
+    public boolean isSubtree(TreeNode root, TreeNode subRoot) {
+        if (root == null) {
+            return false;
+        } else if (compSubTree(root, subRoot)) {
+            return true;
+        }
+        return isSubtree(root.left, subRoot) || isSubtree(root.right, subRoot);
+    }
+
+    private boolean compSubTree(TreeNode root, TreeNode subRoot) {
+        if (root == null && subRoot == null) {
+            return true;
+        } else if (root == null || subRoot == null) {
+            return false;
+        } else if (root.val != subRoot.val) {
+            return false;
+        }
+        return compSubTree(root.left, subRoot.left) && compSubTree(root.right, subRoot.right);
+    }
+
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        return compSame(p, q);
+    }
+
+    private boolean compSame(TreeNode p, TreeNode q) {
+        if (p == null && q == null) {
+            return true;
+        } else if (p == null && q != null) {
+            return false;
+        } else if (p != null && q == null) {
+            return false;
+        } else if (p.val != q.val) {
+            return false;
+        }
+        boolean left = compSame(p.left, q.left);
+        boolean right = compSame(p.right, q.right);
+        return left && right;
+    }
+
+    public static boolean isSymmetric(TreeNode root) {
+        return comp(root.left, root.right);
+    }
+
+    private static boolean comp(TreeNode left, TreeNode right) {
+        if (left == null && right != null) {
+            return false;
+        }
+        if (left != null && right == null) {
+            return false;
+        }
+
+        if (left == null && right == null) {
+            return true;
+        }
+        if (left.val != right.val) {
+            return false;
+        }
+
+        boolean out = comp(left.left, right.right);
+        boolean in = comp(left.right, right.left);
+        return out && in;
+    }
+
+    //前序遍历
+    public static TreeNode invertTree(TreeNode root) {
+        if (root == null) {
+            return root;
+        }
+        TreeNode temp;
+        temp = root.left;
+        root.left = root.right;
+        root.right = temp;
+
+        invertTree(root.left);
+        invertTree(root.right);
+        return root;
     }
 
     public static void main2(String[] args) {
@@ -195,6 +285,7 @@ public class BinaryTree {
         res.add(root.val);
     }
 
+    @ToString
     static class TreeNode {
         public int val;
         public TreeNode left;
