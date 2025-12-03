32fab5bef69090c9c6489b594712ff1d2b0f73d0
fantasy <384130598@qq.com>
2025-12-03 11:00:44 +0800

    ds:代码随想录-动态规划6、7、8

--- datastruct/src/main/java/com/ds/datastruct/dp/DP.java
+++ datastruct/src/main/java/com/ds/datastruct/dp/DP.java
@@ -7,6 +7,10 @@ package com.ds.datastruct.dp;
  */
 public class DP {
     public static void main(String[] args) {
-
+        int[][] obstacleGrid = new int[5][6];
+        int length = obstacleGrid[0].length;
+        System.out.println("length = " + length);
+        int length1 = obstacleGrid.length;
+        System.out.println("length1 = " + length1);
     }
 }
